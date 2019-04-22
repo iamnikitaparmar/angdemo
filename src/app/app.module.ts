46c8170af2c2a2083f85391bd01107f8565f7800
Nikita <nikita.parmar@theopeneyes.in>
2019-04-22 19:54:16 +0530

    ff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { EmployeeComponent } from './employee/employee.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule, 
     FormsModule
   ],
   providers: [],
