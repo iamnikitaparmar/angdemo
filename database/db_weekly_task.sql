271c70eb3b00b8dd0a04194e03a2571b41f34445
Nikita <nikita.parmar@theopeneyes.in>
2019-05-06 19:54:50 +0530

    Update db_weekly_task.sql

--- database/db_weekly_task.sql
+++ database/db_weekly_task.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 01, 2019 at 10:20 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 06, 2019 at 02:23 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,18 +28,29 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbldailytask`
 --
 
-CREATE TABLE `tbldailytask` (
-  `DailyTaskId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tbldailytask`;
+CREATE TABLE IF NOT EXISTS `tbldailytask` (
+  `DailyTaskId` int(11) NOT NULL AUTO_INCREMENT,
   `EmployeeId` int(11) NOT NULL,
   `ProjectId` int(11) NOT NULL,
   `TaskDate` date NOT NULL,
   `TaskDescription` text NOT NULL,
-  `IsActive` bit(1) NOT NULL DEFAULT b'1',
+  `IsActive` tinyint(4) NOT NULL DEFAULT '1',
   `CreatedBy` int(10) NOT NULL,
-  `CreatedDate` date NOT NULL,
+  `CreatedDate` datetime NOT NULL,
   `UpdatedBy` int(10) NOT NULL,
-  `UpdatedOn` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `UpdatedDate` datetime NOT NULL,
+  PRIMARY KEY (`DailyTaskId`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbldailytask`
+--
+
+INSERT INTO `tbldailytask` (`DailyTaskId`, `EmployeeId`, `ProjectId`, `TaskDate`, `TaskDescription`, `IsActive`, `CreatedBy`, `CreatedDate`, `UpdatedBy`, `UpdatedDate`) VALUES
+(1, 1, 1, '2019-05-01', 'fgfhgf', 1, 0, '2019-05-14 00:00:00', 0, '2019-05-16 00:00:00'),
+(2, 3, 8, '2019-05-22', 'dddd', 0, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(4, 1, 1, '2019-05-07', 'ffffffff', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -45,8 +58,9 @@ CREATE TABLE `tbldailytask` (
 -- Table structure for table `tblemployees`
 --
 
-CREATE TABLE `tblemployees` (
-  `EmployeeId` int(10) NOT NULL,
+DROP TABLE IF EXISTS `tblemployees`;
+CREATE TABLE IF NOT EXISTS `tblemployees` (
+  `EmployeeId` int(10) NOT NULL AUTO_INCREMENT,
   `EmployeeName` varchar(100) NOT NULL,
   `JoiningDate` date NOT NULL,
   `BirthDate` date NOT NULL,
@@ -58,8 +72,9 @@ CREATE TABLE `tblemployees` (
   `CreatedBy` int(10) NOT NULL,
   `CreatedDate` datetime NOT NULL,
   `UpdatedBy` int(10) NOT NULL,
-  `UpdatedOn` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `UpdatedOn` datetime NOT NULL,
+  PRIMARY KEY (`EmployeeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tblemployees`
@@ -69,7 +84,7 @@ INSERT INTO `tblemployees` (`EmployeeId`, `EmployeeName`, `JoiningDate`, `BirthD
 (1, 'Nikita P.', '2019-04-04', '2019-04-04', 'ffff', '9428293789', 'php', 'n@g.comm', 1, 1, '2019-04-01 00:00:00', 1, '2019-04-01 00:00:00'),
 (2, 'Nikita Parmar', '2019-04-01', '1989-10-06', 'kutch', '7634567890', 'php', 'n@gh.com', 0, 1, '2019-04-15 00:00:00', 1, '2019-04-15 00:00:00'),
 (3, 'Nikikkkk', '2019-04-03', '2019-04-03', 'ff', 'vgfghh', 'php', 'n@gf.com', 1, 1, '2019-04-10 00:00:00', 1, '2019-04-09 00:00:00'),
-(48, 'Nikita', '2019-05-10', '2019-05-09', 'ffdfd', '9428293789', 'php', 'n@gd.comm', 0, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
+(49, 'Nikita Parmar', '2019-05-08', '2019-05-08', 'hjk', '7634567890', 'php', 'nkl@gmail.com', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -77,70 +92,30 @@ INSERT INTO `tblemployees` (`EmployeeId`, `EmployeeName`, `JoiningDate`, `BirthD
 -- Table structure for table `tblprojects`
 --
 
-CREATE TABLE `tblprojects` (
-  `ProjectId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tblprojects`;
+CREATE TABLE IF NOT EXISTS `tblprojects` (
+  `ProjectId` int(11) NOT NULL AUTO_INCREMENT,
   `ProjectName` varchar(150) NOT NULL,
   `Description` text NOT NULL,
-  `IsActive` bit(1) NOT NULL DEFAULT b'1',
-  `CreatedBy` int(10) NOT NULL,
-  `CreatedDate` date NOT NULL,
-  `UpdatedBy` int(10) NOT NULL,
-  `UpdatedOn` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `IsActive` tinyint(4) NOT NULL DEFAULT '1',
+  `CreatedBy` int(11) NOT NULL,
+  `CreatedDate` datetime NOT NULL,
+  `UpdatedBy` int(11) NOT NULL,
+  `UpdatedDate` datetime NOT NULL,
+  PRIMARY KEY (`ProjectId`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `tblprojects`
 --
 
---
--- Indexes for table `tbldailytask`
---
-ALTER TABLE `tbldailytask`
-  ADD PRIMARY KEY (`DailyTaskId`),
-  ADD KEY `EmployeeId` (`EmployeeId`),
-  ADD KEY `ProjectId` (`ProjectId`);
-
---
--- Indexes for table `tblemployees`
---
-ALTER TABLE `tblemployees`
-  ADD PRIMARY KEY (`EmployeeId`);
-
---
--- Indexes for table `tblprojects`
---
-ALTER TABLE `tblprojects`
-  ADD PRIMARY KEY (`ProjectId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbldailytask`
---
-ALTER TABLE `tbldailytask`
-  MODIFY `DailyTaskId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `tblemployees`
---
-ALTER TABLE `tblemployees`
-  MODIFY `EmployeeId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
---
--- AUTO_INCREMENT for table `tblprojects`
---
-ALTER TABLE `tblprojects`
-  MODIFY `ProjectId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tbldailytask`
---
-ALTER TABLE `tbldailytask`
-  ADD CONSTRAINT `tbldailytask_ibfk_1` FOREIGN KEY (`EmployeeId`) REFERENCES `tblemployees` (`EmployeeId`),
-  ADD CONSTRAINT `tbldailytask_ibfk_2` FOREIGN KEY (`ProjectId`) REFERENCES `tblprojects` (`ProjectId`);
+INSERT INTO `tblprojects` (`ProjectId`, `ProjectName`, `Description`, `IsActive`, `CreatedBy`, `CreatedDate`, `UpdatedBy`, `UpdatedDate`) VALUES
+(1, 'projecttttt', 'ffgggggg', 0, 0, '2019-05-02 00:00:00', 0, '2019-05-02 00:00:00'),
+(2, 'project', 'ffgg', 1, 0, '2019-05-02 00:00:00', 0, '2019-05-02 00:00:00'),
+(6, 'hghjghj', 'vvhjvhj', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(7, 'fhgfhgfhg', 'fyugfyug', 0, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(8, 'prjdgfdg', 'fgfhgf', 1, 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
